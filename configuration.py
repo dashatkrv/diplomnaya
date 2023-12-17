a7db7da108a210647fd60cda19dc5269329da4c4
dachatkrv <dachatkrv@gmail.com>
2023-12-18 03:52:50 +0400

    Initial commit

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://d532c93a-804b-4dfc-94d8-4db1d6d48ee4.serverhub.praktikum-services.ru/"
+CREATE_NEW_ORDER = "/api/v1/orders"
+RECEIVING_ORDER_BY_ID = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Daria",
+    "lastName": "Tokareva",
+    "address": "Ekb, VIZ ",
+    "metroStation": 4,
+    "phone": "+7 982 675 25 35",
+    "rentTime": 5,
+    "deliveryDate": "2023-12-12",
+    "comment": "hi, bro",
+    "color": [
+        "BLACK"
+    ]
+}
